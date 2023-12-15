24180a93acaf3db9cf29e10f6dee6189cc0d7b94
Daniel Wang <dwwwang@berkeley.edu>
2023-12-14 16:57:51 -0800

    feat: add block title and description

--- frontend/src/components/Columns/BrowseColumn.js
+++ frontend/src/components/Columns/BrowseColumn.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from '../../styles/BrowseColumn.module.css';
 
-const ColumnBlock = ({ imageUrl }) => {
+const ColumnBlock = ({ imageUrl, text }) => {
     return (
         <>
             <div className={styles.columnBlock}>
                 <img src={imageUrl} alt="Block Content" className={styles.columnBlockImage} />
+                <div className={styles.blockTitle}>{text[0]}</div>
+                <div className={styles.blockDescription}>{text[1]}</div>
             </div>
         </>
     );
@@ -22,7 +24,7 @@ const BrowseColumn = () => {
     return (
         <div className={styles.browseColumn}>
           {images.map((image, index) => (
-            <ColumnBlock key={index} imageUrl={image} />
+            <ColumnBlock key={index} imageUrl={image} text={["BASIC CREW", "STUSSY"]} />
           ))} {/* JavaScript expression embedded in JSX (how React mixes HTML-like code with JavaScript) */}
         </div>
     );
