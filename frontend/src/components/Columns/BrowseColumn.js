0bf6e4b94eed9ae3cd095509b544ebcb99c54327
Daniel Wang <dwwwang@berkeley.edu>
2024-01-01 17:33:19 -0800

    style: change sizing and positioning

--- frontend/src/components/Columns/BrowseColumn.js
+++ frontend/src/components/Columns/BrowseColumn.js
@@ -4,7 +4,7 @@ import styles from '../../styles/BrowseColumn.module.css';
 const ColumnBlock = ({ imageUrl, text }) => {
     return (
         <>
-            <div className={styles.columnBlock}>
+            <div className={styles.browseColumnBlock}>
                 <img src={imageUrl} alt="Block Content" className={styles.columnBlockImage} />
                 <div className={styles.blockTitle}>{text[0]}</div>
                 <div className={styles.blockDescription}>{text[1]}</div>
@@ -31,5 +31,3 @@ const BrowseColumn = () => {
 };
 
 export default BrowseColumn;
-
-{/* <p>hi my name danyo and i have no balls in my free time i like cuddling w my homies yey :3 yay yay omg thank you omgomgomgomgm thank you thank you omgomgogmogm do you want bobar bobar :33333 curse word hastage %%%% hastage hasgsate how many phonemes are in hasta ge yes ok i am good at coding anf lohy publush this at psych . hi my name danyo and i have no balls in my free time i like cuddling w my homies yey :3 yay yay omg thank you omgomgomgomgm thank you thank you omgomgogmogm do you want bobar bobar :33333 curse word hastage %%%% hastage hasgsate how many phonemes are in hasta ge yes ok i am good at coding anf lohy publush this at psych . hi my name danyo and i have no balls in my free time i like cuddling w my homies yey :3 yay yay omg thank you omgomgomgomgm thank you thank you omgomgogmogm do you want bobar bobar :33333 curse word hastage %%%% hastage hasgsate how many phonemes are in hasta ge yes ok i am good at coding anf lohy publush this at psych . hi my name danyo and i have no balls in my free time i like cuddling w my homies yey :3 yay yay omg thank you omgomgomgomgm thank you thank you omgomgogmogm do you want bobar bobar :33333 curse word hastage %%%% hastage hasgsate how many phonemes are in hasta ge yes ok i am good at coding anf lohy publush this at psych . hi my name danyo and i have no balls in my free time i like cuddling w my homies yey :3 yay yay omg thank you omgomgomgomgm thank you thank you omgomgogmogm do you want bobar bobar :33333 curse word hastage %%%% hastage hasgsate how many phonemes are in hasta ge yes ok i am good at coding anf lohy publush this at psych .</p> */}
